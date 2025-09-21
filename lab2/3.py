abe9643545319f67ddd769b05989c205344e3333
lidiya <karavaeva.lidaa@yandex.ru>
2025-09-21 14:20:49 +0700

    lab2

---
+++ lab2/3.py
@@ -0,0 +1,9 @@
+def del_all_e(s, e):
+    if s == '':
+        return ''
+    if s[0] == e:
+        return del_all_e(s[1:], e)
+    else:
+        return s[0]+ del_all_e(s[1:], e)
+
+print(del_all_e('мама мыла раму', 'а'))
